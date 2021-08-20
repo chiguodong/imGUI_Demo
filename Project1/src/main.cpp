775261dcc314848521e7c799f09efca2d78bb873
unknown <zhangtianrui@soulapp.com>
2021-08-20 13:40:29 +0800

    [add] solve the opengl env & add first imgui text

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,5 +1,8 @@
+#include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
-#include "imgui.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 int main(void)
 {
@@ -8,7 +11,6 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -16,22 +18,57 @@ int main(void)
 		glfwTerminate(); 
 		return -1;
 	}
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	bool err = gl3wInit() != 0;
+	// Imgui context
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		//imgui
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::End();
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+
 		glClearColor(255, 255, 0, 255);
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
-		glfwPollEvents();
 	}
 
 	glfwTerminate();
